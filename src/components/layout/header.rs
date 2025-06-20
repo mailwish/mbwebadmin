9db53dc45843de23ab65f463d36fc7af04b8f74a
Ravgrg <99405975+ravivgolov@users.noreply.github.com>
2025-06-20 10:16:01 +0300

    Update header.rs

--- src/components/layout/header.rs
+++ src/components/layout/header.rs
@@ -190,15 +190,6 @@ pub fn Header(permissions: Memo<Option<Permissions>>) -> impl IntoView {
                                             <IconAdjustmentsHorizontal/>
                                             Configure
                                         </a>
-
-                                        <a
-                                            class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                                            href="https://github.com/sponsors/stalwartlabs"
-                                            target="_blank"
-                                        >
-                                            <IconHeart/>
-                                            Sponsor
-                                        </a>
                                     </div>
 
                                 </div>
